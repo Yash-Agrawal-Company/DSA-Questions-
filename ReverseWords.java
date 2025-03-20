16befa0817a7f0267b262955c6dcb6c8145f30ed
Yash Agrawal <yashagrawal9319029562@gmail.com>
2025-03-20 13:41:43 +0530

    12 completed

---
+++ ReverseWords.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class ReverseWords {
+    public static void main(String[] args) {
+        String s = "a good   example";
+        s = s.trim();
+        String words[] = s.split(" ");
+        StringBuffer result = new StringBuffer();
+        for(int i = words.length - 1 ; i >= 0 ; i--){
+            if(words[i].length() < 1){
+                continue;
+            }
+            result.append(words[i]);
+            if (i > 0)  {
+                result.append(" ");
+            }
+        }
+        System.out.println(result.toString());
+    }
+}
