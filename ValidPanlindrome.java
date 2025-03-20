5216927f7293b9bc18ae6a5c0670631515eee636
Yash Agrawal <yashagrawal9319029562@gmail.com>
2025-03-20 12:05:39 +0530

    11 completed

--- ValidPanlindrome.java
+++ ValidPanlindrome.java
@@ -1,5 +1,18 @@
 public class ValidPanlindrome {
     public static void main(String[] args) {
-        
+        String s = "A man, a plan, a canal: Panama";
+        String output;
+        s = s.toLowerCase();
+        StringBuffer bf = new StringBuffer();
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isLetterOrDigit(s.charAt(i))) {
+                bf.append(s.charAt(i));
+            }
+        }
+
+        StringBuffer org = bf;
+        StringBuffer reversed = new StringBuffer(bf).reverse();
+        output = org.toString().equals(reversed.toString()) ?  "Valid palindrome" : "Invalid palindrome";
+        System.out.println(output);
     }
 }
