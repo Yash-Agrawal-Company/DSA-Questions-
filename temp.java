1775b6b77167c9b2d41909359cece1b3822b2005
Yash Agrawal <yashagrawal9319029562@gmail.com>
2025-03-21 22:07:56 +0530

    deleted

--- temp.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-
-public class temp {
-    public static void main(String[] args) {
-        int arr[] = {3,1,2,6,3};
-        Arrays.sort(arr);
-        System.out.println(arr);
-    }
-}
